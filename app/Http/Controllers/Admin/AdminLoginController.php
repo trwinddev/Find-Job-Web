6385cf8cad285638899cffefe8b79c057532502d
Phong NT <>
2023-08-30 22:23:09 +0700

    handle login and logout

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Admin;
 use App\Mail\Websitemail;
 use Illuminate\Support\Facades\Hash;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -15,8 +15,35 @@ class AdminLoginController extends Controller
     {
         return view('admin.login');
     }
+
     public function forget_password()
     {
         return view('admin.forget_password');
+    }
+
+    public function login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (Auth::guard('admin')->attempt($credential)) {
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Email or password is incorrect!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', function () {
 });
 
 // Admin
-Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
 Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
+Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
 Route::get('/admin/forget_password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
